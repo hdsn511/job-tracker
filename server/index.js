67a14df1a246ae13e271e671fdd6e6ca7953dd80
htreinen <htreinen@wisc.edu>
2026-03-19 17:52:20 -0500

    fixes

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://your-job-tracker-frontend-ten-eta.vercel.app"
+    "https://job-tracker-frontend-ten-eta.vercel.app"
   ]
 }));
 app.use(express.json());
