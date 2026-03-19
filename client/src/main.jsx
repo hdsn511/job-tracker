2cfaa2d9425cf8bb4fbae9005e040bfb489f3d5b
htreinen <htreinen@wisc.edu>
2026-03-19 18:09:10 -0500

    handled route edge cases

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from '@/pages/Dashboard'
 import  Login from '@/pages/Login'
+import ProtectedRoute from "@/components/ProtectedRoute";
 import './index.css'
 
 
@@ -11,7 +12,11 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
